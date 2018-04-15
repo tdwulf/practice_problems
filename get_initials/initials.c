c043f8853d460f67aadd5239738b5ca702e284cd
Wulfie <revwulf@gmail.com>
2018-04-14 19:32:56 -0700

    initials_c

---
+++ get_initials/initials.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main(void)
+{
+    char name[50];
+    //get user input
+    fgets(name, 50 ,stdin);
+    printf("%s",name);
+    //itterate through each character of the string
+    for(int i = 0; name[i] != '\0'; i++)
+    {
+        //check to see if the character is a NOT a space AND is either preceded by a blank or is the first character in the string
+        if(name[i] != (char)32) {
+            if (name[i-1] == (char)32 || name[i] == name[0])
+            {
+                 //Print the intials in uppercase
+            printf("%c",toupper(name[i]));
+            }
+        } 
+    }
+    printf("\n");
+}
